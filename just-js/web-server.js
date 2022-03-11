1a306ec2c56277b456f5608cfe442f55d867cad2
Neelabh Raman <neelabhraman@gmail.com>
2022-03-11 12:38:22 +0530

    adding formatting And updating git ignore file

--- just-js/web-server.js
+++ just-js/web-server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const fs=require('fs');
-const port =9100;
+const fs = require('fs');
+const port = 9100;
 
 /*const express = require('express');
 const path = require('path');
@@ -34,25 +34,25 @@ const _MimeMap = {
     'ico': 'image/x-icon'
 };
 
-const server=http.createServer(function(req,res){
-console.log("Now requesting for resource:",req.url);
-    res.writeHead(200,{'Content-Type': _MimeMap[req.url.split('.').pop()] || 'text/html'});
-    fs.readFile(req.url.split('/').pop() || "index.html",function(error,data){
-        if(error){
+const server = http.createServer(function (req, res) {
+    console.log("Now requesting for resource:", req.url);
+    res.writeHead(200, {'Content-Type': _MimeMap[req.url.split('.').pop()] || 'text/html'});
+    fs.readFile(req.url.split('/').pop() || "index.html", function (error, data) {
+        if (error) {
             res.writeHead(404);
             res.write("Error: HTML file not found");
-        }else{
+        } else {
             res.write(data);
         }
         res.end();
     });
 });
 
-server.listen(port,function(error){
+server.listen(port, function (error) {
 
-    if(error){
+    if (error) {
         console.log("Error in starting node server", error);
-    }else{
+    } else {
         console.log("Node server started at port", port);
     }
 
