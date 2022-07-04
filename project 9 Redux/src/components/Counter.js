b644549961046bb4ba668d5da5ab8f995b294935
Neelabh Raman <neelabhraman@gmail.com>
2022-07-04 12:30:55 +0530

    managing multiple items in store

--- project 9 Redux/src/components/Counter.js
+++ project 9 Redux/src/components/Counter.js
@@ -5,26 +5,32 @@ import {useSelector,useDispatch} from "react-redux";
 const Counter = () => {
     const counter=useSelector(state => state.counter);
     const dispatch=useDispatch();
-
+    const showCount=useSelector(state => state.showCount);
     const incrementCounterHandler = () => {
         dispatch({type:"increment"});
     };
-
+    const increaseHandler = () => {
+        dispatch({type:"increase",value:5});
+    };
     const decrementCounterHandler = () => {
         dispatch({type:"decrement"});
     };
 
-    const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+
+        dispatch({type:"toggle"});
+    };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+        {showCount && <div className={classes.value}>{counter}</div>}
         <div>
             <button onClick={incrementCounterHandler}>Increment</button>
+            <button onClick={increaseHandler}>Increase by 5</button>
             <button onClick={decrementCounterHandler}>Decrement</button>
         </div>
-      {/*<button onClick={toggleCounterHandler}>Toggle Counter</button>*/}
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
--- project 9 Redux/src/store/index.js
+++ project 9 Redux/src/store/index.js
@@ -1,12 +1,20 @@
 
 import  {createStore} from 'redux';
 
-const myReducer= (state={counter:0},action)=>{
+const myReducer= (state={counter:0,showCount:true},action)=>{
 
     if(action.type==="increment"){
-        return {counter:state.counter + 1};
+        return {counter:state.counter + 1,
+            showCount:state.showCount};
     }else if(action.type==="decrement"){
-        return {counter:state.counter -1 };
+        return {counter:state.counter -1 ,
+            showCount:state.showCount};
+    }else if(action.type==="increase"){
+        return {counter:state.counter +action.value ,
+            showCount:state.showCount};
+    }else if(action.type==="toggle"){
+        return {counter:state.counter  ,
+            showCount: !state.showCount};
     }
 
     return state;
